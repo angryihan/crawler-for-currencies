16308d9b024137d23f0b7f8d993ae865204b4035
angryihan <495552532@qq.com>
2016-10-17 13:25:50 +0800

    optimize index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,40 +10,46 @@ var endInput = document.querySelector("#endDate");
 var pjSelect = document.querySelector("#pjname");
 var isGenerating = false;
 
-generateBtn.addEventListener("click", generateHandler);
-openDirBtn.addEventListener("click", openDirHandler);
-endInput.addEventListener("keydown", function(event) {
-    if (event.keyCode == 13) {
-        generateBtn.click();
-    }
-});
-
-function generateHandler() {
-    if (!isGenerating) {
-        disableBtn();
-        async.series({
-            getCurrencies: function(cb) {
-                crawler.getCurrencies(dateHelper.getDateArray(startInput.value, endDate.value), pjSelect.value, cb);
-            },
-            resetBtn: function() {
-                enableBtn();
+var currenciedCrawler = {
+    init: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        var self = this;
+        generateBtn.addEventListener("click", self.generateHandler);
+        openDirBtn.addEventListener("click", self.openDirHandler);
+        endInput.addEventListener("keydown", function(event) {
+            if (event.keyCode == 13) {
+                self.generateHandler();
             }
-        }, function(err) {
-            console.log(err);
         });
+    },
+    generateHandler: function() {
+        if (!isGenerating) {
+            currenciedCrawler.disableBtn();
+            async.series({
+                getCurrencies: function(cb) {
+                    crawler.getCurrencies(dateHelper.getDateArray(startInput.value, endDate.value), pjSelect.value, cb);
+                },
+                resetBtn: function() {
+                    currenciedCrawler.enableBtn();
+                }
+            }, function(err) {
+                console.log(err);
+            });
+        }
+    },
+    openDirHandler: function() {
+        fileHelper.openDir();
+    },
+    enableBtn: function() {
+        isGenerating = false;
+        generateBtn.text = "一键获取";
+    },
+    disableBtn: function() {
+        isGenerating = true;
+        generateBtn.text = "获取中，请稍候……";
     }
 }
 
-function openDirHandler() {
-    fileHelper.openDir();
-}
-
-function enableBtn() {
-    isGenerating = false;
-    generateBtn.text = "一键获取";
-}
-
-function disableBtn() {
-    isGenerating = true;
-    generateBtn.text = "获取中，请稍候……";
-}
+currenciedCrawler.init();
